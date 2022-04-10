92553da01c2b0b3eb9720565409c1bfd7b3a6dc4
Christian Eduardo Rivera Tello <ce.rivera@globant.com>
2022-04-09 20:48:46 -0500

    account management microservice with spring boot

---
+++ photoappapiaccountmanagement/src/test/java/com/globant/photoapp/api/account/photoappapiaccountmanagement/PhotoappapiaccountmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.photoapp.api.account.photoappapiaccountmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapiaccountmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
