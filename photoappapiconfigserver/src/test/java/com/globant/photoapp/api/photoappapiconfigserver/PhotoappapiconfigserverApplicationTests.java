5195db2d50d5c4bd6e01f66c64ef039d8d0af981
Christian Eduardo Rivera Tello <ce.rivera@globant.com>
2022-04-09 20:44:35 -0500

    config server microservice with spring cloud config server

---
+++ photoappapiconfigserver/src/test/java/com/globant/photoapp/api/photoappapiconfigserver/PhotoappapiconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globant.photoapp.api.photoappapiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapiconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
